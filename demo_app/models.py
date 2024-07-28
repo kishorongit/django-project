c99538b81941788a793374a7071048907ef7ca76
Kishor Subedi <subedi.kishor1987@gmail.com>
2024-07-28 13:38:25 +0545

    Add Teacher model to demo_app

--- demo_app/models.py
+++ demo_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    contact = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
