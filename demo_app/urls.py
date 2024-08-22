f3e3ed18d58579341eaae3aa77083f8920485270
Kishor Subedi <subedi.kishor1987@gmail.com>
2024-08-22 19:51:51 +0545

    feat: Add teacher form
    - Change database adapter to mysql
    - Change settings to use .env file

--- demo_app/urls.py
+++ demo_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.IndexView, name="index")
+    path("", views.index_view, name="index"),
+    path("form", views.teacher_form, name="teacher-form")
 ]
--- demo_app/views.py
+++ demo_app/views.py
@@ -1,7 +1,29 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
+from .forms import TeacherForm
+from .models import Teacher
 # Create your views here.
 
 
-def IndexView(request):
+def index_view(request):
     return render(request, "demo_app/index.html")
+
+
+def teacher_form(request):
+    # if this is a POST request we need to process the form data
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request:
+        form = TeacherForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # save valid and clean data
+            form.save()
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse("index"))
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = TeacherForm()
+        return render(request, "demo_app/forms.html", {"form": form})
